5dc0d992aaaf8ec657804309dfdc3c9e2e2d8f07
Lloyd Atkinson <lloydjatkinson@users.noreply.github.com>
2024-03-21 22:04:11 +0000

    Add files via upload

---
+++ AvaloniaApplication1/TimeSpanConverter.cs
@@ -0,0 +1,34 @@
+﻿using Avalonia.Data.Converters;
+using Avalonia.Data;
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Humanizer;
+
+namespace AvaloniaApplication1
+{
+    public class TimeSpanConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is TimeSpan timeSpan)
+            {
+                if (parameter is string format)
+                {
+                    return timeSpan.Humanize(culture: culture);
+                }
+            }
+
+            return new BindingNotification(new InvalidCastException(), BindingErrorType.Error);
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ AvaloniaApplication1/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace AvaloniaApplication1.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+
+    public TimeSpan Time => TimeSpan.FromMilliseconds(10);
+}
